04c9e9295a789b2c41d33c5c8887f75251d7eda7
marko-acso <marko.marcovic@gmail.com>
2026-03-22 16:45:24 +0200

    Shrink neutral avatar emoji to 20px

---
+++ fix_size2.py
@@ -0,0 +1,4 @@
+﻿c = open('review.html','r',encoding='utf-8').read()
+c = c.replace("font-size:24px;\" title=\"Neutral", "font-size:20px;\" title=\"Neutral")
+open('review.html','w',encoding='utf-8').write(c)
+print('Neutral avatar resized to 20px')
